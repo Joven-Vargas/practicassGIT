b28de965c347286be66e06ebcb45929c79c5e427
JovenVargas <isaac.vargas768@ist17dejulio.edu.ec>
2024-11-28 16:01:36 -0500

    interface

---
+++ src/modelo/FiguraGeometrica.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Asus
+ */
+public interface FiguraGeometrica {
+    
+    
+    /// seclaracion contantante --final 
+    
+    public final double PI =3.1416;
+    
+    
+    
+    
+    //declarar metodos implementados 
+    
+    public void area();
+    public double perimetro();
+    
+    
+    
+    
+    
+}
